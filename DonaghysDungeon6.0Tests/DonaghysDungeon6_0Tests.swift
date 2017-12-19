6e5728db1150e6f82a0805a5e8c51ea038647f84
Chris Walter <walt50@gmail.com>
2017-12-19 10:04:55 -0500

    Initial Commit

---
+++ DonaghysDungeon6.0Tests/DonaghysDungeon6_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  DonaghysDungeon6_0Tests.swift
+//  DonaghysDungeon6.0Tests
+//
+//  Created by Christopher Walter on 12/19/17.
+//  Copyright © 2017 AssistStat. All rights reserved.
+//
+
+import XCTest
+@testable import DonaghysDungeon6_0
+
+class DonaghysDungeon6_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
